55eaeab1e5f9a7296debddf197554e6d0f8aa0cd
Brillo95 <38540342+Brillo95@users.noreply.github.com>
2019-11-27 14:28:22 +0100

    Added a function wich gets all Teams of a Year

--- SWP_Bundesliga/Crawler/crawler_class.py
+++ SWP_Bundesliga/Crawler/crawler_class.py
@@ -3,17 +3,19 @@ import urllib.request
 
 class Crawler:
     def __init__(self, url, year):
-        # URL asu openLigaDB der art https://www.openligadb.de/api/getmatchdata/bl1/
+        # URL asu openLigaDB der art https://www.openligadb.de/api
         self.url = url
 
         # Für welches Jahr gesucht werden soll
         self.year = year
 
+    # holt alle Spiele aus dem VORJAHR
+    # muss noch gemacht werden modifikation sodass ein Zeit Intervall gewählt werden kann
     def get_match_data(self):
 
         # notwending um xml file zu erhalten sonst json
         url_header_xml = urllib.request.Request(
-            url=self.url + str(self.year),
+            url=self.url + "/getmatchdata/bl1/" + str(self.year-1),
             data=None,
             headers={'Content-Type': 'application/xml'}  # änder von json nach xml
         )
@@ -25,7 +27,7 @@ class Crawler:
         soup = bs.BeautifulSoup(page,'xml')
 
         # erstelle eine csv, öffne diese und schreibe die headers
-        filename = "all_games_" + str(self.year) + ".csv"
+        filename = "all_games_" + str(self.year-1) + ".csv"
         f = open(filename, "w")
         headers = "date, team1, team2, goals_team1, goals_team2\n"
         f.write(headers)
@@ -45,4 +47,30 @@ class Crawler:
                 goals_team2 = m.find('MatchResult').find_next('MatchResult').PointsTeam2.text
             f.write(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "\n")
             # print(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2)
+        f.close()
+
+    # holt alle Team Namen des AKTUELLEN Jahres
+    def get_all_teams(self):
+        url_header_xml = urllib.request.Request(
+            url=self.url + "/getavailableteams/bl1/" + str(self.year),
+            data=None,
+            headers={'Content-Type': 'application/xml'}  # änder von json nach xml
+        )
+
+        # öffnet Seite
+        page = urllib.request.urlopen(url_header_xml)
+
+        # Erstellt "xml_soup"
+        soup = bs.BeautifulSoup(page, 'xml')
+
+        # erstelle eine csv, öffne diese und schreibe die headers
+        filename = "all_teams_" + str(self.year) + ".csv"
+        f = open(filename, "w")
+        headers = "team_name, team_short\n"
+        f.write(headers)
+
+        for t in soup.find_all('Team'):
+            team_name = t.TeamName.text
+            team_short = t.ShortName.text
+            f.write(team_name + "," + team_short + "\n")
         f.close()
--- SWP_Bundesliga/Crawler/crawler_test.py
+++ SWP_Bundesliga/Crawler/crawler_test.py
@@ -1,4 +1,5 @@
 import crawler_class
 
-c1 = crawler_class.Crawler("https://www.openligadb.de/api/getmatchdata/bl1/", 2018)
+c1 = crawler_class.Crawler("https://www.openligadb.de/api", 2019)
 c1.get_match_data()
+c1.get_all_teams()
