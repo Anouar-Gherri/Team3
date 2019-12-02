6bda2aa844291ed13b9c5c528ca6d17369b7334c
Darris01 <dav.mueller@student.uni-tuebingen.de>
2019-12-03 00:03:42 +0100

    Completed Algorithm 2; Made few structural changes to algorithm; Completed test files

--- SWP_Bundesliga/Algorithm/AlgorithmClass.py
+++ SWP_Bundesliga/Algorithm/AlgorithmClass.py
@@ -1,47 +1,45 @@
-# Probleme:
-# - Ist die Struktur gut?
-# - Ich bin kein Python Experte- kann man das so machen?
-# - Passen die Error-Typen?
-
-
 # The Algorithm class
-# An Algorithm has the following properties:
+
+
+# An Algorithm takes the following input:
 # - name (string):
-#           a dummy name, e.g. 'RelativeFrequenzyAlgorithm"
-# - training_function (library-file-name crawler-file -> library-file):
-#           a function wich takes the Crawler data and a library-name and trains a
-#           'Libraryfile' with given name. The calculations for the probabilities will later
-#           base on this file
-# - library_name (string):
-#           the filename of the library
-# - trained (boolean):
-#           tells you whether the algorithm has trained or not
-#           (i.e. whether the library exists)
+#           A dummy name, e.g. 'RelativeFrequencyAlgorithm'
+# - training_function (library-filename crawler_data_file *args -> library-file):
+#           a function which takes the Crawler data and trains a
+#           'Library file'. The calculations for the probabilities will later
+#           base on this file. It also has to return a set of the unique teams.
 # - request_function (library-file match-dict -> probability-dict):
 #           will calculate the probability based on the library-file and the match specified
-#           in the match-dict. The calculated probabilites will be returned as a dictionary
-#           with chances for winning, loosing or drawing, refering to the host.
+#           in the match-dict. The calculated probabilities will be returned as a list
+#           with chances for winning, loosing or drawing, referring to the host.
 #           e.g. {"host": "munich", "guest": 'dusseldorf', "Place": "munich",
 #                 "date": "2009-08-07T20:30:00"}
-#                -> {'host': 'Munich', 'win': 0.4, 'lose': 0.4, 'draw': 0.2}
-# - file_type(dictionary):
-#           A small dictionary containing the file-types of the crawler-data and the library.
-#           You cannot train unless your crawler-data-file and libname are/have the according
-#           filetype
+#                -> [0.4, 0.4, 0.2]
+# - data format (string):
+#           the format of the trainings data. has to fit with the trainings function.
+#           All '.' in the string will be deleted
+# - library format (string):
+#           the format of the library. has to fit the trainings function an request function.
+#           All '.' in the string will be deleted
+# - library_name [optional] (str):
+#           A name to determine the name of the library file. Case not given,
+#           <name> wil be taken.
+
 class Algorithm:
     def __init__(self, name: str, training_function, request_function, data_format: str,
-                 library_format: str, library_name: str = '', trained: bool = False):
-        """ Creates a new algorithm.
+                 library_format: str, library_name: str = ''):
+        """Creates a new algorithm.
 
-        :param name: A name for the Algorithm
-        :param training_function: A function using the crawler data to create a 'library'
+        :param name: A name for the Algorithm.
+        :param training_function: A function using the crawler data to create a 'library'.
+               It has to return the set of the unique teams
         :param request_function: A function using the library and match information
-        to predict its outcomes
+               to predict its outcomes
         :param data_format: The file type of the crawler data
         :param library_format: The file type of the library
-        :param library_name: The filename of the library. Will be overwritten
-                             when .train() is used.
-        :param trained: A boolean to state whether the Algorithm has trained or not
+        :param library_name: An optional input to generate Library files with unique names.
+               Case not given, Library_<name> will be used for this.
+               Eg: library_name: 'Library_RFA' -> library_filename: Library_RFA.<library_format>
 
         :type name: str
         :type training_function: function
@@ -49,66 +47,68 @@ class Algorithm:
         :type data_format: str
         :type library_format: str
         :type library_name: str
-        :type trained: bool
 
         :rtype: object (Algorithm)
         """
         self.name = name
         self.training_function = training_function
-        self.library_name = library_name
-        self.trained = trained
         self.request_function = request_function
+
+        data_format = data_format.replace('.', '')
+        library_format = library_format.replace('.', '')
+        library_name = self.name if library_name == '' else library_name
+
         self.file_types = dict(crawler=data_format, library=library_format)
+        self.library_filename = 'Library_{}.{}'.format(library_name, library_format)
+        self.trained = False
+        self.teams = set()
 
     # --- Set Properties ---
-    def set_name(self, name):
+    def set_name(self, name: str):
         self.name = name
 
     def set_training_function(self, training_function):
         self.training_function = training_function
 
-    def set_library(self, library_name):
-        self.library_name = library_name
-
-    def set_trained(self, trained):
-        self.trained = trained
-
     def set_request_function(self, request_function):
         self.request_function = request_function
 
-    def set_file_types(self, file_types):
+    def set_file_types(self, file_types: dict):
         self.file_types = file_types
 
-    # --- Common Functions ---
-    # Trains the Library
-    # It will also set obj.library_name to lib_name and obj.trained = true
-    def train(self, lib_name, crawler_data_file_name, *args):
-        """Trains the algorithm. The function will use a file called
-        crawler_crawler_data_file_name to create a library called lib_name
+    def set_library_filename(self, library_filename: str):
+        self.library_filename = library_filename
 
-        :param lib_name: The name of the created library
+    def set_trained(self, trained: bool):
+        self.trained = trained
+
+    def set_teams(self, teams: set):
+        self.teams = teams
+
+    # --- Common Functions ---
+    # Trains the Library (sets obj.trained = true)
+    def train(self, crawler_data_file_name, *args):
+        """Trains the algorithm. The function will use a file called
+        crawler_crawler_data_file_name to create a library called self.library_name.
+        Sets the teams attribute.
+
         :param crawler_data_file_name: The name of the data-file used to create the library
         :param args: additional arguments the training_function may use
 
-        :type lib_name: str
         :type crawler_data_file_name: str
         """
-        if not crawler_data_file_name.endswith(self.file_types["crawler"]):
+        data_format = self.file_types['crawler']
+        # Generate the name of the Library from the Algorithm name and Library ending
+        if not crawler_data_file_name.endswith(data_format):
             raise ValueError(
-                "The type of the requested crawler-data-file does not match"
-                "the expected type :" + self.file_types["crawler"])
-
-        if not lib_name.endswith(self.file_types["library"]):
-            raise ValueError(
-                "The type of the library name does not match"
-                "the expected type :" + self.file_types["crawler"])
+                "The type of the requested crawler-data-file does not match the expected type :" + data_format)
 
         with open(crawler_data_file_name, "r") as data:
-            self.training_function(lib_name, data, *args)
+            teams = self.training_function(self.library_filename, data, *args)
         data.close()
 
-        self.library_name = lib_name
         self.trained = True
+        self.teams = teams
 
     # Requests a match
     # Output type: {'host': '---', 'win': --%, 'lose': --%, 'draw': --%}
@@ -119,13 +119,39 @@ class Algorithm:
         :param match_dict: A dictionary specifying the match. The information
         you'll need to specify depends on the information the request_function needs
         :type match_dict: dict
+
         :return: a dictionary containing the host and his chances of winning, loosing or drawing
         """
+        host = match_dict['host']
+        guest = match_dict['guest']
 
         if not self.trained:
-            raise NameError('The library "' + self.library_name
-                            + '" has to be trained first')
+            raise NameError('The library "{}" has to be trained first (Use self.train)'.format(self.library_filename))
+        # TODO: teams cannot handle ä, ö, ü and maybe other not aswell
+        #
+        if host not in self.teams:
+            raise ValueError("\"{}\" is not in the listed teams!".format(host))
+        if guest not in self.teams:
+            raise ValueError("\"{}\" is not in the listed teams!".format(guest))
 
-        with open(self.library_name, "r") as lib:
-            return self.request_function(lib, match_dict)
+        with open(self.library_filename, "r") as lib:
+            results = self.request_function(lib, match_dict)
         lib.close()
+
+        result_dict = results_to_dict(host, results)
+        return result_dict
+
+
+# Helper functions
+def results_to_dict(host, results):
+    if not len(results) == 3:
+        raise ValueError('Result list has to contain 3 entries')
+    # Every entry has to be a positive number
+    if not all(map(lambda x: isinstance(x, (int, float)) and x >= 0, results)):
+        raise ValueError('Result list is not numeric')
+    if not sum(results) == 1:
+        raise ValueError('Result list is not normalized')
+
+    res_dict = {'host': host}
+    res_dict.update({case: res for case, res in zip(["win", "lose", "draw"], results)})
+    return res_dict
