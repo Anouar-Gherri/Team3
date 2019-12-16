02ebc3299fb2813f084dcd8e72c612105eec8e20
Brillo95 <38540342+Brillo95@users.noreply.github.com>
2019-12-16 21:57:25 +0100

    New improved Crawler with tests for get_teams

--- SWP_Bundesliga/Crawler/crawler_class.py
+++ SWP_Bundesliga/Crawler/crawler_class.py
@@ -1,204 +1,119 @@
-# coding=utf-8
-import bs4 as bs  # BeautifulSoup4 Packet
 import urllib.request
+import pandas as pd
 
 
 class Crawler:
+
     def __init__(self, url):
-        # URL asu openLigaDB der art https://www.openligadb.de/api
         self.url = url
 
-        # Für welches Jahr gesucht werden soll
+    def get_data(self, year, data, s_day, e_day):
+        """Loads and stores a portion the wanted data into a dictionary and returns the dictionary.
+        Always gets entire year which gets filter by s_day and e_day
 
-    # holt alle Spiele aus einem Jahr
-    # muss noch gemacht werden modifikation sodass ein Zeit Intervall gewählt werden kann
-    def get_match_data(self, year):
+         :param year: The year which the current data origins
+         :param data: A Dictionary which holds already collected data and where the new data gets stored into
+         :param s_day: Starting play day for the requested data needed
+         :param e_day: Ending play day for the requested data needed
 
-        # notwending um xml file zu erhalten sonst json
-        url_header_xml = urllib.request.Request(
+         :type year: int
+         :type data: Dictionary
+         :type s_day: int
+         :type e_day: int
+
+         :return: a dictionary which contains the already collected data and the new data from the year \n"""
+
+        url_header = urllib.request.Request(
             url=self.url + "/getmatchdata/bl1/" + str(year),
             data=None,
-            headers={'Content-Type': 'application/xml'}  # änder von json nach xml
+            headers={'Content-Type': 'application/json'}
         )
-
-        # öffnet Seite
-        page = urllib.request.urlopen(url_header_xml)
-
-        # Erstellt "xml_soup"
-        soup = bs.BeautifulSoup(page, 'xml')
-
-        # erstelle eine csv, öffne diese und schreibe die headers
-        filename = "all_games_" + str(year) + ".csv"
-        f = open(filename, "w")
-        headers = "date, team1, team2, goals_team1, goals_team2, is_Finished , round \n"
-        f.write(headers)
-
-        # liest aus jedem gefundenem Match date, team1 ,team2 ,pointsTeam1 und pointsTeam2
-        for m in soup.find_all('Match'):
-            date=str(m.MatchDateTime.text)
-            team1 = m.Team1.TeamName.text
-            team2 = m.Team2.TeamName.text
-            is_Finished = m.MatchIsFinished.text
-            round = m.Group.GroupOrderID.text
-            # If zum unterscheiden von endständen und halbzeitergebnissen da diese keine "vorgegebene Folge" haben
-            if m.MatchIsFinished.text == "true":
-              if m.find('ResultName').text == "Endergebnis":
-                goals_team1 = m.MatchResults.MatchResult.PointsTeam1.text
-                goals_team2 = m.MatchResults.MatchResult.PointsTeam2.text
-              else:
-                goals_team1 = m.find('MatchResult').find_next('MatchResult').PointsTeam1.text
-                goals_team2 = m.find('MatchResult').find_next('MatchResult').PointsTeam2.text
-              f.write(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n" )
-            else:
-                goals_team1 = ''
-                goals_team2 = ''
-                f.write( date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                 # print(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 "," + is_Finished )
-        f.close()
-
-    # holt alle Spiele aus dem angegebenen Interval "s_" Anfangs Jahr/tag, "e_" End Jahr/Tag
-
+        page = urllib.request.urlopen(url_header)
+        matches = pd.read_json(page)
+        for m in range(0, 306):
+            if matches['Group'][m]['GroupOrderID'] in range(s_day, e_day + 1):
+                data['date'].append(matches['MatchDateTime'][m])
+                data['team1'].append(matches['Team1'][m]['TeamName'])
+                data['team2'].append(matches['Team2'][m]['TeamName'])
+                data['is_finished'].append(matches['MatchIsFinished'][m])
+                data['play_day'].append(matches['Group'][m]['GroupOrderID'])
+                if matches['MatchIsFinished'][m]:
+                    if matches['MatchResults'][m][0]['ResultName'] == "Endergebnis":
+                        data['goal1'].append(matches['MatchResults'][m][0]['PointsTeam1'])
+                        data['goal2'].append(matches['MatchResults'][m][0]['PointsTeam2'])
+                    else:
+                        data['goal1'].append(matches['MatchResults'][m][1]['PointsTeam1'])
+                        data['goal2'].append(matches['MatchResults'][m][1]['PointsTeam2'])
+                else:
+                    data['goal1'].append("-")
+                    data['goal2'].append("-")
+        return data
 
     def get_match_data_interval(self, s_year, s_day, e_year, e_day):
-     first=True
-     filename="all_games_" + str(s_year) + "." + str(s_day) + "-" + str(e_year) + "." + str(e_day) \
-             + ".csv "
-     f=open(filename, "w")
-     headers="date, team1, team2, goals_team1, goals_team2, is_Finished , round \n"
-     f.write(headers)
-     for y in range(s_year, e_year + 1):
-        if y == e_year:
-            for d in range(1, e_day + 1):
-                url_header_xml=urllib.request.Request(
-                    url=self.url + "/getmatchdata/bl1/" + str(y) + "/" + str(d),
-                    data=None,
-                    headers={'Content-Type': 'application/xml'}  # änder von json nach xml
-                )
-                # öffnet Seite
-                page=urllib.request.urlopen(url_header_xml)
+        """Searches the match data which are requested. First stores all data into
+        a dictionary then when all data is collected Stores all the data into matches.csv File
 
-                # Erstellt "xml_soup"
-                soup=bs.BeautifulSoup(page, 'xml')
-                # liest aus jedem gefundenem Match date, team1 ,team2 ,pointsTeam1 und pointsTeam2
-                for m in soup.find_all('Match'):
-                    date=str(m.MatchDateTime.text)
-                    team1=m.Team1.TeamName.text
-                    team2=m.Team2.TeamName.text
-                    is_Finished=m.MatchIsFinished.text
-                    round=m.Group.GroupOrderID.text
-                    # If zum unterscheiden von endständen und halbzeitergebnissen da diese keine "vorgegebene Folge" haben
-                    if m.MatchIsFinished.text == "true":
-                        if m.find('ResultName').text == "Endergebnis":
-                            goals_team1=m.MatchResults.MatchResult.PointsTeam1.text
-                            goals_team2=m.MatchResults.MatchResult.PointsTeam2.text
-                        else:
-                            goals_team1=m.find('MatchResult').find_next('MatchResult').PointsTeam1.text
-                            goals_team2=m.find('MatchResult').find_next('MatchResult').PointsTeam2.text
-                        f.write(
-                            date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                    else:
-                        goals_team1=''
-                        goals_team2=''
-                        f.write(
-                            date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                        # print(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 "," + is_Finished )
-            f.close()
+        :param s_year: Specifies the starting year for the data
+        :param s_day: Specifies the starting day in s_year
+        :param e_year: Specifies the ending year for the data
+        :param e_day: Specifies the ending day in e_year
+
+        :type s_year: int
+        :type s_day: int
+        :type e_year: int
+        :type e_day: int \n"""
+
+        data = {'date': [],
+                'team1': [],
+                'team2': [],
+                'is_finished': [],
+                'play_day': [],
+                'goal1': [],
+                'goal2': []}
+        if s_year == e_year:
+            self.get_data(s_year, data, s_day, e_day)
         else:
-            if first:
-                for d in range(s_day, 35):
-                    url_header_xml=urllib.request.Request(
-                        url=self.url + "/getmatchdata/bl1/" + str(y) + "/" + str(d),
-                        data=None,
-                        headers={'Content-Type': 'application/xml'}  # änder von json nach xml
-                    )
-                    # öffnet Seite
-                    page=urllib.request.urlopen(url_header_xml)
+            for y in range(s_year, e_year + 1):
+                if y == s_year:
+                    self.get_data(y, data, s_day, 34)
+                else:
+                    if y == e_year:
+                        self.get_data(y, data, 1, e_day)
+                    else:
+                        self.get_data(y, data, 1, 34)
+        df = pd.DataFrame(data, columns=['date', 'team1', 'team2', 'goal1', 'goal2', 'is_finished', 'play_day'])
+        df.to_csv('matches.csv', index=False)
 
-                    # Erstellt "xml_soup"
-                    soup=bs.BeautifulSoup(page, 'xml')
-                    # liest aus jedem gefundenem Match date, team1 ,team2 ,pointsTeam1 und pointsTeam2
-                    for m in soup.find_all('Match'):
-                        date=str(m.MatchDateTime.text)
-                        team1=m.Team1.TeamName.text
-                        team2=m.Team2.TeamName.text
-                        is_Finished=m.MatchIsFinished.text
-                        round=m.Group.GroupOrderID.text
-                        # If zum unterscheiden von endständen und halbzeitergebnissen da diese keine "vorgegebene Folge" haben
-                        if m.MatchIsFinished.text == "true":
-                            if m.find('ResultName').text == "Endergebnis":
-                                goals_team1=m.MatchResults.MatchResult.PointsTeam1.text
-                                goals_team2=m.MatchResults.MatchResult.PointsTeam2.text
-                            else:
-                                goals_team1=m.find('MatchResult').find_next('MatchResult').PointsTeam1.text
-                                goals_team2=m.find('MatchResult').find_next('MatchResult').PointsTeam2.text
-                            f.write(
-                                date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                        else:
-                            goals_team1=''
-                            goals_team2=''
-                            f.write(
-                                date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                            # print(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 "," + is_Finished )
-                first=False
-            else:
-                for d in range(1, 35):
-                    url_header_xml=urllib.request.Request(
-                        url=self.url + "/getmatchdata/bl1/" + str(y) + "/" + str(d),
-                        data=None,
-                        headers={'Content-Type': 'application/xml'}  # änder von json nach xml
-                    )
-                    # öffnet Seite
-                    page=urllib.request.urlopen(url_header_xml)
+    def get_teams(self, s_year, e_year):
+        """ Stores all Teams which played between s_year and e_year in bl1
+        in teams.csv
 
-                    # Erstellt "xml_soup"
-                    soup=bs.BeautifulSoup(page, 'xml')
-                    # liest aus jedem gefundenem Match date, team1 ,team2 ,pointsTeam1 und pointsTeam2
-                    for m in soup.find_all('Match'):
-                        date=str(m.MatchDateTime.text)
-                        team1=m.Team1.TeamName.text
-                        team2=m.Team2.TeamName.text
-                        is_Finished=m.MatchIsFinished.text
-                        round=m.Group.GroupOrderID.text
-                        # If zum unterscheiden von endständen und halbzeitergebnissen da diese keine "vorgegebene Folge" haben
-                        if m.MatchIsFinished.text == "true":
-                            if m.find('ResultName').text == "Endergebnis":
-                                goals_team1=m.MatchResults.MatchResult.PointsTeam1.text
-                                goals_team2=m.MatchResults.MatchResult.PointsTeam2.text
-                            else:
-                                goals_team1=m.find('MatchResult').find_next('MatchResult').PointsTeam1.text
-                                goals_team2=m.find('MatchResult').find_next('MatchResult').PointsTeam2.text
-                            f.write(
-                                date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                        else:
-                            goals_team1=''
-                            goals_team2=''
-                            f.write(
-                                date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 + "," + is_Finished + "," + round + "\n")
-                            # print(date + "," + team1 + "," + team2 + "," + goals_team1 + "," + goals_team2 "," + is_Finished )
+         :param s_year: Specifies starting year for the Team data
+         :param e_year: Specifies ending year for the Team data
+
+         :type s_year: int
+         :type e_year: int """
+
+        team_dict = {'name': [],
+                     'year': []}
+        for y in range(s_year, e_year + 1):
+            url_header = urllib.request.Request(
+                url=self.url + "/getavailableteams/bl1/" + str(y),
+                data=None,
+                headers={'Content-Type': 'application/json'}
+            )
+            page = urllib.request.urlopen(url_header)
+            teams = pd.read_json(page)
+            for i in range(0, 18):
+                team_dict['name'].append(teams['TeamName'][i])
+                team_dict['year'].append(y)
+        df = pd.DataFrame(team_dict, columns=['name', 'year'])
+        df.to_csv('teams.csv', index=False)
 
 
-    def get_all_teams(self, year):
-        url_header_xml = urllib.request.Request(
-            url=self.url + "/getavailableteams/bl1/" + str(year),
-            data=None,
-            headers={'Content-Type': 'application/xml'}  # änder von json nach xml
+# print(Crawler.get_teams.__doc__)
+# print(Crawler.get_data.__doc__)
+# print(Crawler.get_match_data_interval.__doc__)
 
-         )
-
-        # öffnet Seite
-        page = urllib.request.urlopen(url_header_xml,timeout=1)
-
-        # Erstellt "xml_soup"
-        soup = bs.BeautifulSoup(page, 'xml')
-
-        # erstelle eine csv, öffne diese und schreibe die headers
-        filename = "all_teams_" + str(year) + ".csv"
-        f = open(filename, "w")
-        headers = "team_name, team_short\n"
-        f.write(headers)
-
-        for t in soup.find_all('Team'):
-            team_name = t.TeamName.text
-            team_short = t.ShortName.text
-            f.write(team_name + "," + team_short + "\n")
-        f.close()
+# c = Crawler("https://www.openligadb.de/api")
+# c.get_match_data_interval(2016, 1, 2016, 1)
--- SWP_Bundesliga/Crawler/crawler_test.py
+++ SWP_Bundesliga/Crawler/crawler_test.py
@@ -1,13 +1,42 @@
 import crawler_class
+import pytest
+import pandas as pd
 
-c1 = crawler_class.Crawler("https://www.openligadb.de/api")
 
-# alle Spiele aus dem Jahr 2015
-c1.get_match_data(2016)
+@pytest.fixture(scope='module')
+def crawler():
+    c = crawler_class.Crawler("https://www.openligadb.de/api")
+    return c
 
-# alle Teams in der BL im Jahr 2015
-# c1.get_all_teams(2015)
 
-# alle Spiele vom Jahr 2011 Spieltag 1 bis zum Jahr 2012 Spieltag 17
-c1.get_match_data_interval(2011, 1, 2014, 17)
+@pytest.mark.parametrize('start_year, end_year',
+                         [
+                             (2016, 2016),
+                             (2016, 2018),
+                         ])
+def test_get_teams(crawler, start_year, end_year):
+    crawler.get_teams(start_year, end_year)
+    df = pd.read_csv('teams.csv')
+    assert len(df) == (end_year - start_year + 1) * 18
+    assert type(df) is pd.DataFrame
+    assert pd.read_csv('teams.csv').equals(pd.read_csv('teams_' + str(start_year) + '_' + str(end_year) + '.csv'))
 
+
+@pytest.mark.parametrize('year, start_day, end_day',
+                         [
+                             (2016, 1, 1),
+                             (2015, 16, 18)
+                         ])
+def test_get_data(crawler, year, start_day, end_day):
+    data = {'date': [],
+            'team1': [],
+            'team2': [],
+            'is_finished': [],
+            'play_day': [],
+            'goal1': [],
+            'goal2': []}
+    new_data = crawler.get_data(year, data, start_day, end_day)
+    assert type(new_data) is dict
+    assert len(new_data['date']) == 9 * (end_day - start_day + 1)
+    """need to add data check if correct data inhere
+    also need to add test to get_match_data_intervall"""
